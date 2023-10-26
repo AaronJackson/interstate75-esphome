319e195dcd4d4cbeb789f3a490267c8a524fa707
Aaron Jackson <aaron@aaronsplace.co.uk>
2023-10-26 01:09:49 +0100

    trying dual core

--- components/hub75display/hub75display.cpp
+++ components/hub75display/hub75display.cpp
@@ -1,5 +1,7 @@
 #include "hub75display.h"
 
+#include "pico/mutex.h"
+
 namespace esphome {
   namespace hub75display {
     static const char *const TAG = "hub75display.display";
@@ -10,6 +12,51 @@ namespace esphome {
 
     void HUB75Display::set_writer(hub75display_writer_t &&writer) {
       this->writer_ = writer;
+    }
+
+    static mutex_t lock;
+
+    uint32_t ximg[64*64];
+    void core1_redraw() {
+      uint32_t owner;
+      int h = 64 / 2;
+      int w = 64;
+
+      while (1) {
+	for (int row = 0; row < h; row++) {
+	  for (int col = 0; col < w; col++) {
+	    int i0 = row * 64 + col;
+	    int i1 = (row + 32) * 64 + col;
+
+	    if (!mutex_try_enter(&lock, &owner))
+	      mutex_enter_blocking(&lock);
+	    digitalWrite(I75_R0, (ximg[i0] & 0x0000FF) > 0);
+	    digitalWrite(I75_G0, (ximg[i0] & 0x00FF00) > 0);
+	    digitalWrite(I75_B0, (ximg[i0] & 0xFF0000) > 0);
+	    digitalWrite(I75_R1, (ximg[i1] & 0x0000FF) > 0);
+	    digitalWrite(I75_G1, (ximg[i1] & 0x00FF00) > 0);
+	    digitalWrite(I75_B1, (ximg[i1] & 0xFF0000) > 0);
+	    mutex_exit(&lock);
+
+	    digitalWrite(I75_CLK, HIGH);
+	    digitalWrite(I75_CLK, LOW);
+	  }
+
+	  digitalWrite(I75_OE, HIGH);
+
+	  digitalWrite(I75_ROW_A, (row & 1) > 0);
+	  digitalWrite(I75_ROW_B, (row & 2) > 0);
+	  digitalWrite(I75_ROW_C, (row & 4) > 0);
+	  digitalWrite(I75_ROW_D, (row & 8) > 0);
+	  digitalWrite(I75_ROW_E, (row & 16) > 0);
+
+	  digitalWrite(I75_STB, HIGH);
+	  digitalWrite(I75_STB, LOW);
+
+	  digitalWrite(I75_OE, LOW);
+	}
+      }
+
     }
 
     void HUB75Display::setup() {
@@ -30,6 +77,9 @@ namespace esphome {
 
       digitalWrite(I75_OE, LOW);
       digitalWrite(I75_STB, LOW);
+
+      mutex_init(&lock);
+      multicore_launch_core1(core1_redraw);
     }
 
     void HUB75Display::update() {
@@ -39,45 +89,17 @@ namespace esphome {
       this->display();
     }
 
+
     void HUB75Display::display() {
-      int h = this->get_height_internal() / 2;
-      int w = this->get_width_internal();
-
-
-      for (int row = 0; row < h; row++) {
-	for (int col = 0; col < w; col++) {
-	  int i0 = row * 64 + col;
-	  int i1 = (row + 32) * 64 + col;
-
-	  digitalWrite(I75_R0, (this->img[i0] & 0x0000FF) > 0);
-	  digitalWrite(I75_G0, (this->img[i0] & 0x00FF00) > 0);
-	  digitalWrite(I75_B0, (this->img[i0] & 0xFF0000) > 0);
-	  digitalWrite(I75_R1, (this->img[i1] & 0x0000FF) > 0);
-	  digitalWrite(I75_G1, (this->img[i1] & 0x00FF00) > 0);
-	  digitalWrite(I75_B1, (this->img[i1] & 0xFF0000) > 0);
-
-	  digitalWrite(I75_CLK, HIGH);
-	  digitalWrite(I75_CLK, LOW);
-	}
-
-	digitalWrite(I75_OE, HIGH);
-
-	digitalWrite(I75_ROW_A, (row & 1) > 0);
-	digitalWrite(I75_ROW_B, (row & 2) > 0);
-	digitalWrite(I75_ROW_C, (row & 4) > 0);
-	digitalWrite(I75_ROW_D, (row & 8) > 0);
-	digitalWrite(I75_ROW_E, (row & 16) > 0);
-
-	digitalWrite(I75_STB, HIGH);
-	digitalWrite(I75_STB, LOW);
-
-	digitalWrite(I75_OE, LOW);
-      }
 
     }
 
+    uint32_t c0owner;
     void HUB75Display::draw_absolute_pixel_internal(int x, int y, Color c) {
-      this->img[y * 64 + x] = c.raw_32;
+      if (!mutex_try_enter(&lock, &c0owner))
+	mutex_enter_blocking(&lock);
+      ximg[y * 64 + x] = c.raw_32;
+      mutex_exit(&lock);
     }
 
     int HUB75Display::get_height_internal() {
